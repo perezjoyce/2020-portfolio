184f7a2f6bc9d3363fc97c16472fa63edc7c95a1
japerez <japerez.ph@gmail.com>
2019-02-26 03:59:48 +0800

    frontend edits

---
+++ assets/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    // $('.sidenav').sidenav();
+    // $('.parallax').parallax();
+    // $('.modal').modal();
+    M.AutoInit();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
