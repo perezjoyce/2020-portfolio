ae44831c3bc292917283acf47e84bd990fddf626
unknown <japerez.ph@gmail.com>
2020-08-01 17:37:53 +0800

    done with edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    window.onscroll = function() {scrollFunction()};
+document.addEventListener("DOMContentLoaded", function (event)
+{
+    window.onscroll = function () { scrollFunction() }
 
     //when page scrolls down, show btn-resume
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            document.getElementById("btn-resume").style.display = "block";
-        } else {
-            document.getElementById("btn-resume").style.display = "none";
+    function scrollFunction ()
+    {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+        {
+            document.getElementById("btn-resume").style.display = "block"
+        } else
+        {
+            document.getElementById("btn-resume").style.display = "none"
         }
     }
-});
+})
